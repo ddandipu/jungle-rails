1b797711c89fdcb04c8271f3c041a9b0bb7f749b
ddandipu <andiputra.dimas@hotmail.com>
2017-06-09 22:57:53 +0000

    set routes to a signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-
+  get '/signup', to: 'users#new'
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
